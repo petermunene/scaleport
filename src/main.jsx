da244326480d20eb06d6f5ba1d8c6d677393457f
petermunene <peter.munene@student.moringaschool.com>
2025-06-12 08:32:09 +0300

    lll

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Import this
+import { HashRouter } from 'react-router-dom'; // ✅ Changed this
 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
